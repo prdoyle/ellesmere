c50ac4e767491d5d83ef05ba560a197897590fd7
patdoyle <patdoyle@8a67d1f4-d520-4d41-b900-46b8824da3b7>
2010-12-15 05:01:40 +0000

    Stop handing out pointers to Productions that are allocated in memory subject to realloc.
    git-svn-id: file:///svn/ellesmere@36 8a67d1f4-d520-4d41-b900-46b8824da3b7

--- grammar.c
+++ grammar.c
@@ -1,6 +1,7 @@
 
 #include "grammar.h"
 #include "memory.h"
+#include <stdint.h>
 
 typedef struct pe_struct
 	{
@@ -15,50 +16,16 @@ typedef struct rhs_struct *RightHandSide;
 #undef AR_BYVALUE
 #include "array_template.h"
 
-struct pn_struct
+typedef struct pns_struct
 	{
 	Symbol lhs;
 	RightHandSide rhs;
-	};
-
-FUNC void pr_appendWithName( Production pn, Symbol name, Symbol token, Grammar gr )
-	{
-	ProductionElement pe;
-	rhs_incCount( pn->rhs );
-	pe = rhs_last( pn->rhs, 0 );
-	pe->token = token;
-	pe->name  = name;
-	}
-
-FUNC void pr_stopAppending( Production pn, Grammar gr )
-	{
-	rhs_setCapacity( pn->rhs, rhs_count( pn->rhs ) );
-	}
-
-FUNC Symbol pr_lhs( Production pn, Grammar gr )
-	{
-	return pn->lhs;
-	}
-
-FUNC int pr_length( Production pn, Grammar gr )
-	{
-	return rhs_count( pn->rhs );
-	}
-
-FUNC Symbol pr_token( Production pn, int index, Grammar gr )
-	{
-	return rhs_element( pn->rhs, index )->token;
-	}
-
-FUNC Symbol pr_name( Production pn, int index, Grammar gr )
-	{
-	return rhs_element( pn->rhs, index )->token;
-	}
+	} *ProductionStorage;
 
 typedef struct pra_struct *ProductionArray;
 #define AR_PREFIX  pra
 #define AR_TYPE    ProductionArray
-#define AR_ELEMENT struct pn_struct
+#define AR_ELEMENT struct pns_struct
 #undef AR_BYVALUE
 #include "array_template.h"
 
@@ -67,6 +34,53 @@ struct gr_struct
 	Symbol goal;
 	ProductionArray pra;
 	};
+
+// Production "references" are actually indexes within the ProductionArray,
+// because the latter can move if it gets resized.
+
+static inline Production pns2pn( ProductionStorage pns, Grammar gr )
+	{
+	return (Production)( pns - pra_element( gr->pra, 0 ) );
+	}
+
+static inline ProductionStorage pn2pns( Production pn, Grammar gr )
+	{
+	return pra_element( gr->pra, (int)(intptr_t)pn );
+	}
+
+FUNC void pr_appendWithName( Production pn, Symbol name, Symbol token, Grammar gr )
+	{
+	ProductionElement pe;
+	rhs_incCount( pn2pns(pn,gr)->rhs );
+	pe = rhs_last( pn2pns(pn,gr)->rhs, 0 );
+	pe->token = token;
+	pe->name  = name;
+	}
+
+FUNC void pr_stopAppending( Production pn, Grammar gr )
+	{
+	rhs_setCapacity( pn2pns(pn,gr)->rhs, rhs_count( pn2pns(pn,gr)->rhs ) );
+	}
+
+FUNC Symbol pr_lhs( Production pn, Grammar gr )
+	{
+	return pn2pns(pn,gr)->lhs;
+	}
+
+FUNC int pr_length( Production pn, Grammar gr )
+	{
+	return rhs_count( pn2pns(pn,gr)->rhs );
+	}
+
+FUNC Symbol pr_token( Production pn, int index, Grammar gr )
+	{
+	return rhs_element( pn2pns(pn,gr)->rhs, index )->token;
+	}
+
+FUNC Symbol pr_name( Production pn, int index, Grammar gr )
+	{
+	return rhs_element( pn2pns(pn,gr)->rhs, index )->token;
+	}
 
 FUNC Grammar gr_new( Symbol goal, int numProductionsEstimate )
 	{
@@ -88,17 +102,17 @@ FUNC void gr_stopAdding( Grammar gr )
 
 FUNC Production gr_production( Grammar gr, int index )
 	{
-	return pra_element( gr->pra, index );
+	return pns2pn( pra_element( gr->pra, index ), gr );
 	}
 
 FUNC Production pr_new( Grammar gr, Symbol lhs, int lengthEstimate )
 	{
-	Production result;
+	ProductionStorage result;
 	pra_incCount( gr->pra );
 	result = pra_last( gr->pra, 0 );
 	result->lhs = lhs;
 	result->rhs = rhs_new( lengthEstimate );
-	return result;
+	return pns2pn( result, gr );
 	}
 
 // MERGE:22
