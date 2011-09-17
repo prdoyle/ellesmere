fc6cedbea99b1d85715619f183d8e5e3455957c8
patdoyle <patdoyle@8a67d1f4-d520-4d41-b900-46b8824da3b7>
2011-09-17 00:07:33 +0000

    Turn Symbols into just masquerading symbol indexes so that the symbol table can now be correctly resized without making existing Symbol references wrong.
    git-svn-id: file:///svn/ellesmere@154 8a67d1f4-d520-4d41-b900-46b8824da3b7

--- symbols.c
+++ symbols.c
@@ -5,23 +5,23 @@
 #include <string.h>
 #include <stdint.h>
 
-struct sy_struct
+typedef struct sys_struct
 	{
 	const char *name;
 	Object token;
 	Record instanceShape;
-	};
+	} *SymbolStorage;
 
 #define AR_PREFIX  sta
 #define AR_TYPE    SymbolTable
-#define AR_ELEMENT struct sy_struct
+#define AR_ELEMENT struct sys_struct
 #undef AR_BYVALUE
 #include "array_template.h"
 #ifndef NDEBUG
 	#define sta_new( size, ml ) sta_newAnnotated( size, ml, __FILE__, __LINE__ )
 #endif
 
-static struct sy_struct predefinedSymbols[] =
+static struct sys_struct predefinedSymbols[] =
 	{
 	{ "NULL_SYMBOL" },
 	{ "INT" },
@@ -48,12 +48,37 @@ static struct sy_struct predefinedSymbols[] =
 	{ "DELEGATE" },
 	};
 
+enum { FIRST_SYMBOL_INDEX=0 }; // We want NULL_SYMBOL to correspond to zero
+
+FUNC SymbolIndex sy_index( Symbol sy, SymbolTable st )
+	{
+	SymbolIndex result = (SymbolIndex)( FIRST_SYMBOL_INDEX + ((intptr_t)sy) );
+	assert( 0 <= result && result < st_count( st ) );
+	return result;
+	}
+
+FUNC Symbol sy_byIndex( SymbolIndex index, SymbolTable st )
+	{
+	assert( 0 <= index && index < st_count(st) );
+	return (Symbol)(intptr_t)index;
+	}
+
+static SymbolStorage sy2sys( Symbol sy, SymbolTable st )
+	{
+	return sta_element( st, sy_index( sy, st ) );
+	}
+
+static Symbol sys2sy( SymbolStorage sys, SymbolTable st )
+	{
+	return sy_byIndex( sys - sta_element( st, 0 ), st );
+	}
+
 FUNC SymbolTable theSymbolTable()
 	{
 	static SymbolTable result = NULL;
 	if( !result )
 		{
-		result = sta_new( 1000 + NUM_PREDEFINED_SYMBOLS, ml_singleton() );
+		result = sta_new( 100 + NUM_PREDEFINED_SYMBOLS, ml_singleton() );
 		sta_setCount( result, NUM_PREDEFINED_SYMBOLS );
 		memcpy( sta_element( result, 0 ), predefinedSymbols, sizeof(predefinedSymbols) );
 		}
@@ -65,46 +90,32 @@ FUNC SymbolIndex st_count( SymbolTable st )
 	return sta_count( st );
 	}
 
-FUNC Symbol sy_byIndex( SymbolIndex index, SymbolTable st )
-	{
-	// TODO: BAH!  This doesn't work if the array resizes itself
-	assert( index < st_count(st) );
-	return sta_element( st, index );
-	}
-
 FUNC Symbol sy_byName( const char *name, SymbolTable st )
 	{
-	Symbol sy;
 	SymbolIndex i;
 	for( i=0; i < st_count(st); i++ )
 		if(!strcmp( name, sy_name( sy_byIndex(i,st), st ) ))
 			return sy_byIndex(i,st);
-	sy = sta_nextElement( st );
-	memset( sy, 0, sizeof(*sy) );
-	sy->name = strdup( name );
+	SymbolStorage sys = sta_nextElement( st );
+	memset( sys, 0, sizeof(*sys) );
+	sys->name = strdup( name );
 	// TODO: Add an inheritance relation with ANY
-	return sy;
-	}
-
-FUNC SymbolIndex sy_index( Symbol sy, SymbolTable st )
-	{
-	assert( sta_element( st, 0 ) <= sy && sy <= sta_element( st, sta_count(st)-1 ) );
-	return sy - sta_element( st, 0 );
+	return sys2sy( sys, st );
 	}
 
 FUNC const char *sy_name( Symbol sy, SymbolTable st )
 	{
-	return sy->name;
+	return sy2sys( sy, st )->name;
 	}
 
 FUNC Record sy_instanceShape( Symbol sy, SymbolTable st )
 	{
-	return sy->instanceShape;
+	return sy2sys( sy, st )->instanceShape;
 	}
 
 FUNC void sy_setInstanceShape( Symbol sy, Record rd, SymbolTable st )
 	{
-	sy->instanceShape = rd;
+	sy2sys( sy, st )->instanceShape = rd;
 	}
 
 FUNC int sy_sendTo( Symbol sy, File fl, SymbolTable st )
@@ -118,9 +129,10 @@ FUNC int sy_sendTo( Symbol sy, File fl, SymbolTable st )
 
 FUNC Object oh_symbolToken( ObjectHeap heap, Symbol sy )
 	{
-	if( !sy->token )
-		sy->token = ob_createToken( sy, heap );
-	return sy->token;
+	SymbolStorage sys = sy2sys( sy, oh_fieldSymbolTable( heap ) );
+	if( !sys->token )
+		sys->token = ob_createToken( sy, heap );
+	return sys->token;
 	}
 
 //MERGE:20
